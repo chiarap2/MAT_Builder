fb4d7772fa6996aab6d2515cd7fbf8e2dc01c12d
chiara pugliese <chiara.pugliese@phd.unipi.it>
2022-05-24 21:07:20 +0200

    user interface css

--- mat_builder.py
+++ mat_builder.py
@@ -11,25 +11,43 @@ import numpy as np
 from core.backend import *
 import json
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = JupyterDash(__name__,external_stylesheets=external_stylesheets)
+app = JupyterDash(__name__)#external_stylesheets=external_stylesheets)
 
 tabs_styles = {
     'height': '44px'
 }
 tab_style = {
-    'borderBottom': '1px solid #d6d6d6',
+    'borderBottom': '1px solid #ddc738',
+    'borderLeft': '0px',
+    'borderRight': '0px',
+    'borderTop': '0px',
     'padding': '6px',
-    'fontWeight': 'bold'
+    'fontWeight': 'bold',
+    'backgroundColor': '#131313'
 }
 
 tab_selected_style = {
-    'borderTop': '1px solid #d6d6d6',
-    'borderBottom': '1px solid #d6d6d6',
-    'backgroundColor': '#119DFF',
-    'color': 'white',
+    'borderTop': '1px solid',
+    'borderLeft': '1px solid',
+    'borderRight': '1px solid',
+    'borderBottom': '0px',
+    'borderColor': '#ddc738',
+    'color': '#ddc738',
+    'backgroundColor': '#131313',
     'padding': '6px'
+}
+
+disabled_style = {
+    'borderBottom': '1px solid #ddc738',
+    'borderLeft': '0px',
+    'borderRight': '0px',
+    'borderTop': '0px',
+    'padding': '6px',
+    'fontWeight': 'bold',
+    'backgroundColor': '#131313',
+    'color': '#5d5d5d'
 }
 
 modules = [cls for cls in demo.__subclasses__()]
@@ -41,9 +59,12 @@ index = 0
 for module in modules:
     
     methods = [ {'label':cls.__name__,'value':cls.__name__} for cls in module.__subclasses__()]
-    children_tabs.append(dcc.Tab(id=str(index),label=module.__name__,value=module.__name__,style=tab_style,selected_style=tab_selected_style,
+    if index == 0:
+        children_tabs.append(dcc.Tab(id=str(index),label=module.__name__,value=module.__name__,style=tab_style,selected_style=tab_selected_style,disabled_style=disabled_style,
                                  children=[dcc.RadioItems(id={'type':'radio_items','index':index}, options=methods),html.Button(id={'type':'button','index':index}, children='Submit')]))
-
+    else:
+        children_tabs.append(dcc.Tab(id=str(index),label=module.__name__,value=module.__name__,style=tab_style,selected_style=tab_selected_style,disabled=True, disabled_style=disabled_style,
+                                 children=[dcc.RadioItems(id={'type':'radio_items','index':index}, options=methods),html.Button(id={'type':'button','index':index}, children='Submit')]))
     index += 1
 
 app.layout = html.Div([
@@ -52,7 +73,8 @@ app.layout = html.Div([
     
     html.Div(id='inputs',children=[
         dcc.Tabs(id="tabs-inline", children=children_tabs, style=tabs_styles),
-        html.Hr(),
+        html.Br(),
+        html.Br(),
         html.Div(id='display'),
         html.Button(id='run',children='RUN',style={'display':'none'})],
         style={'float':'left','width':'40%'}),
@@ -65,16 +87,20 @@ app.layout = html.Div([
         html.Div(id='users',children=[html.P(children='Users:'),
             dcc.Dropdown(id='user_list')
         ],style={'display':'none'}),
-        html.Hr(),
+        html.Br(),
+        html.Br(),
         html.Div(id='outputs2'),
         html.Div(id='users_',children=[html.P(children='Users:'),
             dcc.Dropdown(id='user_list_')
         ],style={'display':'none'}),
+        html.Br(),
+        html.Br(),
+        html.Div(id='outputs3'),
+        html.Br(),
+        html.Br(),
         html.Div(id='trajs',children=[html.P(children='Trajectories:'),
             dcc.Dropdown(id='trajs_list')
         ],style={'display':'none'}),
-        html.Hr(),
-        html.Div(id='outputs3'),
         html.Div(id='output-maps'),
     ]),
     
@@ -110,21 +136,36 @@ def show_input(tab,radio):
         for p in parameters:
             
             for elem in p:
-        
+                
+                if elem == 'H5':
+
+                    inputs.append(html.H5(children=p[elem]['children']))                    
+
+                if elem == 'Span':
+
+                    inputs.append(html.Span(children=p[elem]['children']))
+
                 if elem == 'Input':
 
                     inputs.append(dcc.Input(id={'type':'input','index':c},type=p[elem]['type'],placeholder=p[elem]['placeholder']))
-                    inputs.append(html.Hr())
+                    inputs.append(html.Br())
+                    inputs.append(html.Br())
 
                 elif elem == 'Checklist':
 
                     inputs.append(dcc.Checklist(id={'type':'input','index':c},options=p[elem]['options']))
 
                 elif elem == 'Dropdown':
+                    if p[elem]['id'] == 'list_poi':
+                        print('hello')
+                        inputs.append(dcc.Dropdown(id={'type':'input','index':c},options=p[elem]['options'],multi=True,style={'color':'#333'}))                    
+                        inputs.append(html.Br())
+                        inputs.append(html.Br())
 
-                    inputs.append(dcc.Dropdown(id={'type':'input','index':c},options=p[elem]['options'],multi=True))                    
-                    inputs.append(html.Hr())
-
+                    else:
+                        inputs.append(dcc.Dropdown(id={'type':'input','index':c},options=p[elem]['options'],style={'color':'#333'}))                    
+                        inputs.append(html.Br())
+                        inputs.append(html.Br())
 
             c +=1
 
@@ -139,6 +180,9 @@ def show_input(tab,radio):
     Output(component_id='user_list',component_property='options'),
     Output(component_id='users_', component_property='style'),
     Output(component_id='user_list_',component_property='options'),
+    Output(component_id='0', component_property='disabled'),
+    Output(component_id='1', component_property='disabled'),
+    Output(component_id='2', component_property='disabled'),
     State(component_id={'type':'radio_items','index':ALL}, component_property='value'),
     State(component_id={'type':'input','index':ALL}, component_property='value'),
     State(component_id='tabs-inline', component_property='value'),
@@ -150,21 +194,25 @@ def show_output(radio,inputs,tab,click):
     #
     # try to use current triggered in order to raise error (state null value)
 
-    #print(radio, inputs, tab)
+    disable1 = True
+    disable2 = True
     outputs = []
     options = []
     options2 = []
     display = {'display':'none'}
     display2 = {'display':'none'}
 
-    #print(dash.callback_context.triggered)
-    #print()
+    if radio!=[]:
 
+        disable0 = True
 
     if click is None:
-        return None,outputs,display,options,display2,options2
+        disable0 = False
+        return None,outputs,display,options,display2,options2,disable0,disable1,disable2
 
     is_empty = False
+
+    len(inputs)
 
     for input in inputs:
 
@@ -190,6 +238,8 @@ def show_output(radio,inputs,tab,click):
         outputs.append(dcc.Graph(figure=px.histogram(class_pp.df.datetime,x='datetime')))
 
         class_pp.output()
+        disable0 = True
+        disable1 = False
 
     elif tab == 'Segmentation':
 
@@ -202,6 +252,9 @@ def show_output(radio,inputs,tab,click):
 
         display = {'display':'inline'}
         options=[{'label': i, 'value': i} for i in users]
+
+        disable0 = True
+        disable2 = False
 
     elif tab == 'Enrichment':
 
@@ -216,8 +269,10 @@ def show_output(radio,inputs,tab,click):
         display2 = {'display':'inline'}
         options2=[{'label': i, 'value': i} for i in users]
 
+        disable0 = True
+        disable1 = True
 
-    return None,outputs,display,options,display2,options2
+    return None,outputs,display,options,display2,options2,disable0,disable1,disable2
 
 @app.callback(
     Output(component_id='outputs2', component_property='children'),   
@@ -262,7 +317,26 @@ def trajectories(user):
     return display,options
 
 @app.callback(
-    Output(component_id='outputs3', component_property='children'),   
+    Output(component_id='outputs3',component_property='children'),
+    Input(component_id='user_list_',component_property='value')
+)
+
+def info_trajs(users):
+
+    outputs = []
+
+    if users is None:
+
+        return None
+
+    num_systematic = class_e.get_systematic(users)
+    outputs.append(html.Div(children='N. systematic stops: {}'.format(num_systematic)))
+    num_occasional = class_e.get_occasional(users)
+    outputs.append(html.Div(children='N. occasional stops: {}'.format(num_occasional)))
+
+    return outputs
+
+@app.callback(
     Output('output-maps','children'),
     State(component_id='user_list_',component_property='value'),
     Input(component_id='trajs_list',component_property='value')
@@ -276,25 +350,43 @@ def info_enrichment(user,traj):
 
         return None, None
 
-    num_systematic = class_e.get_systematic(user)
-    outputs.append(html.Div(children='N. systematic stops: {}'.format(num_systematic)))
-    num_occasional = class_e.get_occasional(user)
-    outputs.append(html.Div(children='N. occasional stops: {}'.format(num_occasional)))
+    mats_moves, mats_stops, mats_systematic = class_e.get_mats(user,traj)
 
-    mats_moves, mats_stops = class_e.get_mats(user,traj)
-
-    fig = px.line_mapbox(mats_moves, lat="lat", lon="lng", color="tid")
-    print(mats_stops.head())
-    fig.add_trace(go.Scattermapbox(mode = "markers",
-                                   lon = mats_stops.lng,
-                                   lat = mats_stops.lat,
-                                   text = ['amenity'],
-                                   marker = {'size': 10}))
+    fig = px.line_mapbox(mats_moves, lat="lat", lon="lng", color="tid", hover_name="label")
     
-    fig.update_layout(mapbox_style="stamen-terrain", mapbox_zoom=10,
+    mats_stops['distance'] = mats_stops['distance'].astype(str)
+    mats_stops['description'] = mats_stops['category'] + ' ' + mats_stops['distance']
+
+    matched_pois = list(mats_stops.groupby('stop_id')['description'].agg("</br>".join))
+
+    fig.add_trace(go.Scattermapbox(mode = "markers", name = 'occasional stops',
+                                   lon = mats_stops.lng.unique(),
+                                   lat = mats_stops.lat.unique(),
+                                   text = matched_pois,
+                                   hoverinfo = 'text',
+                                   marker = {'size': 10, 'color': '#FF7233'}))
+
+    mats_systematic['home'] = round((mats_systematic['home']*100),2).astype(str)
+    mats_systematic['work'] = round((mats_systematic['work']*100),2).astype(str)
+    mats_systematic['other'] = round((mats_systematic['other']*100),2).astype(str)
+    mats_systematic['frequency'] = mats_systematic['frequency'].astype(str)
+
+    mats_systematic['description'] = '<b> Home </b>: ' + mats_systematic['home'] + '% </br> <b> Work </b>: ' + mats_systematic['work'] + '% </br> <b> Other </b>: ' + mats_systematic['other'] + '% </br> <b> Frequency </b>: ' + mats_systematic['frequency']
+    systematic_desc = list(mats_systematic['description'])
+
+    fig.add_trace(go.Scattermapbox(mode = "markers", name = 'systematic stops',
+                                   lon = mats_systematic.lng,
+                                   lat = mats_systematic.lat,
+                                   text = systematic_desc,
+                                   hoverinfo = 'text',
+                                   marker = {'size': 10,'color': '#74C869'}))
+    
+    fig.update_layout(mapbox_style="stamen-terrain", mapbox_zoom=12,
                       margin={"r":0,"t":0,"l":0,"b":0})
 
-    return outputs,dcc.Graph(figure=fig)   
+    fig.update_traces(line=dict(color='#3392FF',width=2))
+
+    return dcc.Graph(figure=fig)   
 
 
 if __name__ == '__main__':
