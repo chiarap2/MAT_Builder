9c24e4077df58ab2bcf7fb7a6ae925d19e995f9d
Fr4nz83bis <francesco.lettich@humco.it>
2025-03-21 16:21:49 +0100

    Fixed bug concerning the computation of differences between DateTimes, probably due to changes in some modules.

--- core/modules/InteractiveSegmentation.py
+++ core/modules/InteractiveSegmentation.py
@@ -68,7 +68,13 @@ class InteractiveSegmentation(InteractiveModuleInterface):
     def _get_avg_stop_duration(self, uid):
 
         s = self.stops[self.stops['uid'] == uid].copy()
-        s['duration'] = (s['leaving_datetime'] - s['datetime'])
+        
+        # Convert both columns to datetime
+        s['datetime'] = pd.to_datetime(s['datetime'])
+        s['leaving_datetime'] = pd.to_datetime(s['leaving_datetime'])
+    
+        # Calculate duration
+        s['duration'] = s['leaving_datetime'] - s['datetime']
 
         return round(s['duration'].mean().total_seconds() / 60, 2)
 
